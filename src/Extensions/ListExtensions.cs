e017e28e08cdcd54d8432c01bd610e982305bcff
calebjenkins <caleb@calebjenkins.com>
2023-05-27 21:13:21 -0500

    adding list-add-range extension method. Also added ci.yml for running unit tests on non dev or main branches

--- src/Extensions/ListExtensions.cs
+++ src/Extensions/ListExtensions.cs
@@ -1,19 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Calebs.Extensions;
 
 public static class ListExtensions
-{ 
-	public static string ToDelimitedList(this List<string> list, string delimiter)
-	{
-		return string.Join(delimiter, list.ToArray());
-	}
+{
+    public static string ToDelimitedList(this List<string> list, string delimiter)
+    {
+        return string.Join(delimiter, list.ToArray());
+    }
 
-	public static void ToUpper(this List<string> list)
-	{
-		for (var i = 0; i < list.Count; i++)
-		{
-			list[i] = list[i].ToUpper();
-		}
-	}
+    public static void ToUpper(this IList<string> list)
+    {
+        for (var i = 0; i < list.Count; i++)
+        {
+            list[i] = list[i].ToUpper();
+        }
+    }
+
+    public static void AddRange<T>(this IList<T> list, IEnumerable<T> items)
+    {
+        if (list == null) throw new ArgumentNullException(nameof(list));
+        if (items == null) throw new ArgumentNullException(nameof(items));
+
+        if (list is List<T> asList)
+        {
+            asList.AddRange(items);
+        }
+        else
+        {
+            foreach (var item in items)
+            {
+                list.Add(item);
+            }
+        }
+    }
 }
