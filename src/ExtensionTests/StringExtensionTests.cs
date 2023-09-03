d403943feed7ac539dcf478a8de94296ca032fc5
calebjenkins <caleb@calebjenkins.com>
2023-09-03 18:10:23 -0500

    update dev wf - suppress some warnings in the test code.

--- src/ExtensionTests/StringExtensionTests.cs
+++ src/ExtensionTests/StringExtensionTests.cs
@@ -8,7 +8,9 @@ public class StringExtensionTests
     [Fact]
     public void ShouldReturnEmptyStringOnNull()
     {
+#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
         string? value = null;
+#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
         string v2 = value.ValueOrEmpty();
 
         v2.Should().Be(String.Empty);
@@ -17,7 +19,9 @@ public class StringExtensionTests
     [Fact]
     public void ShouldReturnValueIfNotNull()
     {
+#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
         string? value = "hello";
+#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
         string v2 = value.ValueOrEmpty();
 
         v2.Should().Be(value);
