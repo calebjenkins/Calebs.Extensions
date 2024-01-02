277d054e7fcac542ba1e38fdcf61e6e3c66092a5
calebjenkins <caleb@calebjenkins.com>
2024-01-01 22:32:48 -0600

    adding object extensions ToSafeString plus unit tests

---
+++ src/Extensions/ObjectExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Calebs.Extensions;
+
+public static class ObjectExtensions
+{
+    public static string ToSafeString(this object o)
+    {
+        return o?.ToString() ?? string.Empty;
+    }
+}
+
